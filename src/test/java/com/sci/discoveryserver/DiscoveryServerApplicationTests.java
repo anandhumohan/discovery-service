5abbcd3f0164a32779d040861eeb7dab414f982e
anandhumohan <anandhumohan5@gmail.com>
2024-01-30 19:31:25 +0000

    initial commit

---
+++ src/test/java/com/sci/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sci.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
